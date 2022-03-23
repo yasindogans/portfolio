827715974742677b5afcb2b8bff68db0d56cc882
= <=>
2022-03-23 22:27:54 +0300

    project continued

---
+++ src/main/java/com/taysgroup/portfolio/controller/MessageController.java
@@ -0,0 +1,14 @@
+package com.taysgroup.portfolio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @RequestMapping(path = "/message",method = RequestMethod.GET)
+    public String message(){
+        return "{message:\"bu sunucudan aldığım bir mesaj\"}";
+    }
+}
